315e0b68186be748a6c9001a417891450d938eea
LJ <juric.luka@protonmail.com>
2023-09-18 16:39:19 +0200

    first commit

--- cicd/install-terraform.sh
+++ cicd/install-terraform.sh
@@ -1,4 +1,5 @@
 set -eu
+#kann sein dass sudo bei manchen Linux versionen nicht funktioniert, ggf. rausnehmen
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/$release/hashicorp.repo
 sudo yum -y install terraform
